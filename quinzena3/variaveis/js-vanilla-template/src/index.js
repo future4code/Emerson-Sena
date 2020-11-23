e77516773dc3472a3e2f72acc24b895b60e39535
unknown <enunessena@gmail.com>
2020-11-23 18:56:00 -0300

    exercicio aula8

---
+++ quinzena3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,81 @@
+/* // 10 
+10, 5// */
+
+/* 10, 20, 10 */
+
+let nome 
+let idade
+
+
+//Console imprimiu undefined porque a variavel não teve nenhum valor informado
+console.log(typeof nome)
+console.log(typeof idade)
+
+
+//Console imprimiu string na variavel nome e number na variavel idade
+nome = "Emerson"
+idade = 22
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+
+//Console imprimiu o nome e a idade do usuário informadas nas variaveis
+console.log("Olá,", nome, "você tem", idade, "anos") 
+
+
+//Console imprimiu todas as respostas corretas com as informações das variaveis
+let altura = 1.73
+let peso = 63
+let endereço = "Av A, 28"
+let nacionalidade = "Brasileiro"
+let cor = "Azul"
+
+
+console.log("1- Qual sua altura?")
+console.log(altura)
+
+console.log("2- Qual seu peso?")
+console.log(peso)
+
+console.log("3- Qual seu endereço?")
+console.log(endereço)
+
+console.log("4- Qual a sua nacionalidade?")
+console.log(nacionalidade)
+
+console.log("5- Qual sua cor preferida?")
+console.log(cor)
+
+
+//Console imprimiu a array com as comidas em linha
+let comidasFavoritas = ["Arroz", "Feijão", "Strogonoff", "Mousse", "Bolo"]
+
+console.log(comidasFavoritas)
+
+
+//Console imprimiu cada comida em uma linha
+console.log("Essas são minhas comidas preferidas: ")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+
+//Desafio: Console imprimiu a nova lista substituindo a segunda comida
+comidasFavoritas[1] = "Macarrão"
+
+console.log(comidasFavoritas)
+
+//Console imprimiu as perguntas e as respostas no tipo booleano
+let perguntas = ["Você está usando vermelho hoje?", "Você está usando azul hoje?", "Você está usando amarelo hoje?"]
+let respostas = [false, true]
+
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[0])
+
+
+
+
+
