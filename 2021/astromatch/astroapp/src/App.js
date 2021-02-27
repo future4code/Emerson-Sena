7d4b4468eaf9866c6936884dcb9fd1281e8c4495
unknown <enunessena@gmail.com>
2021-02-27 19:56:10 -0300

    astromatch

---
+++ 2021/astromatch/astroapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TelaSelecao from './components/telaSelecao'
+
+function App() {
+  return (
+    <div>
+      <TelaSelecao/>
+    </div>
+  )
+}
+
+export default App;
